cacee24c20d01d7b30923aadcf404239e7efb8c3
Byan70 <odundoachieng@gmail.com>
2024-03-01 21:05:09 +0300

    Python network

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
-"""a Python script that fetches"""
+"""Fetches https://alx-intranet.hbtn.io/status."""
 import urllib.request
-
-url = "https://alx-intranet.hbtn.io/status"
-
-try:
-    with urllib.request.urlopen(url) as response:
+if __name__ == "__main__":
+    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
         body = response.read()
         print("Body response:")
-        print("\t- type:", type(body))
-        print("\t- content:", body)
-        print("\t- utf8 content:", body.decode('utf-8'))
-except Exception as e:
-    print("Error:", e)
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
