ab96ed923bc5fbf109f2305e02870badf5957fca
Byan70 <odundoachieng@gmail.com>
2024-03-01 20:35:45 +0300

    Python network

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
-"""A script that;
-- fetches https://alx-intranet.hbtn.io/status.
-- uses urlib package
-"""
+"""a Python script that fetches"""
+import urllib.request
 
+url = "https://alx-intranet.hbtn.io/status"
 
-if __name__ == '__main__':
-    import urllib.request
-
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
-        content = res.read()
+try:
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
         print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+        print("\t- type:", type(body))
+        print("\t- content:", body)
+        print("\t- utf8 content:", body.decode('utf-8'))
+except Exception as e:
+    print("Error:", e)
